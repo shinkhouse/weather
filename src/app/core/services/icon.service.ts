fb8add0d59ca2de94b0f422a9d15a38ddcef3735
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-06-24 16:20:14 -0500

    add new icons to service

--- src/app/core/services/icon.service.ts
+++ src/app/core/services/icon.service.ts
@@ -3,41 +3,22 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class IconService {
-
     constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-        iconRegistry.addSvgIcon(
-            'sunny',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/sunny.svg'));
-        iconRegistry.addSvgIcon(
-            'partly-cloudy',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/partly-cloudy.svg'));
-        iconRegistry.addSvgIcon(
-            'windy',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/windy.svg'));
-        iconRegistry.addSvgIcon(
-            'cloudy',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/cloudy.svg'));
-        iconRegistry.addSvgIcon(
-            'rainy',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/rainy.svg'));
-        iconRegistry.addSvgIcon(
-            'thunderstorms',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/thunderstorms.svg'));
-        iconRegistry.addSvgIcon(
-            'snowy',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/snowy.svg'));
-        iconRegistry.addSvgIcon(
-            'fog',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/fog.svg'));
-        iconRegistry.addSvgIcon(
-            'hail',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/hail.svg'));
-        iconRegistry.addSvgIcon(
-            'clear-night',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/clear-night.svg'));
+        iconRegistry.addSvgIcon('sunny', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/sunny.svg'));
+        iconRegistry.addSvgIcon('partly-cloudy', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/partly-cloudy.svg'));
+        iconRegistry.addSvgIcon('windy', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/windy.svg'));
+        iconRegistry.addSvgIcon('cloudy', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/cloudy.svg'));
+        iconRegistry.addSvgIcon('rainy', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/rainy.svg'));
+        iconRegistry.addSvgIcon('thunderstorms', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/thunderstorms.svg'));
+        iconRegistry.addSvgIcon('snowy', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/snowy.svg'));
+        iconRegistry.addSvgIcon('fog', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/fog.svg'));
+        iconRegistry.addSvgIcon('hail', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/hail.svg'));
+        iconRegistry.addSvgIcon('clear-night', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/clear-night.svg'));
+        iconRegistry.addSvgIcon('sunrise', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/weather-sunset-up.svg'));
+        iconRegistry.addSvgIcon('sunset', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/weather-sunset-down.svg'));
     }
 
     public iconMapping = {
@@ -53,11 +34,10 @@ export class IconService {
         'partly-cloudy-night': 'partly-cloudy',
         hail: 'hail',
         thunderstorm: 'thunderstorms',
-        tornado: 'tornado'
+        tornado: 'tornado',
     };
 
     getIconFromMapping(icon: string) {
         return this.iconMapping[icon];
     }
-
 }
